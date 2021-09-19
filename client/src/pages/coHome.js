771beb8bc6f678ea033129134c784f05369740fd
NikitaTipule <nikitatipule@gmail.com>
2021-09-19 13:41:14 +0530

    added coordinator info in home page of coordinator

--- client/src/pages/coHome.js
+++ client/src/pages/coHome.js
@@ -10,6 +10,9 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import NavBar from '../components/Navbar/Navbar';
 import { Link } from 'react-router-dom'
+import Grid from '@mui/material/Grid';
+import "../CSS/coHome.css"
+import { borderRadius } from '@mui/system';
 
 
 
@@ -61,106 +64,131 @@ class StickyHeadTable extends Component {
     status: "Verified"
   },
   {
+    id:4,
     name : "nikita sopan Tipule", 
     status: "Not Verified"
   },
   {
+    id:5,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:6,
     name : "nikita sopan Tipule", 
     status: "Modification needed"
   },
 
   {
+    id:7,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:8,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:9,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:10,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:11,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:12,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:13,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:14,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:15,
     name : "nikita sopan Tipule", 
     status: "Not Verified"
   },
   {
+    id:16,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:17,
     name : "nikita sopan Tipule", 
     status: "Modification needed"
   },
 
   {
+    id:18,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:19,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:20,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:21,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:22,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
  
   {
+    id:23,
     name : "nikita sopan Tipule", 
     status: "Modification needed"
   },
 
   {
+    id:24,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:25,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:26,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:27,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
   {
+    id:28,
     name : "nikita sopan Tipule", 
     status: "Verified"
   },
@@ -181,78 +209,144 @@ class StickyHeadTable extends Component {
   return (
     <>
       <NavBar />
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px'}}>
-        <Paper sx={{ width: '80%', overflow: 'hidden'}} >
-          <TableContainer sx={{ maxHeight: 500 }}>
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead >
-                <TableRow>
-                  {this.columns.map((column) => (
-                    <TableCell
-                      key={column.id}
-                      align='center'
-                      style={{ minWidth: column.minWidth, backgroundColor: 'ButtonHighlight', color:'black' }}
-                    >
-                      {column.label}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.rows
-                  .slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
-                  .map((row) => {
-                    return (
-                      <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                        {this.columns.map((column) => {
-                          const value = row[column.id];
-                          return (
-                            <TableCell key={column.id} align='center'>
-                              {column.id === 'status'? (
-                              <div>
-                              {column.id === 'status' && value === 'Verified'
-                                ? (
-                                  <div style = {{color: 'green'}}>{value}</div>
-                                ):(
-                                  <div>
-                                  {column.id === 'status' && value === 'Not Verified'
+      <div>
+        <div>
+          <div style={{display:'block', justifyContent:'center', alignItems:'center'}}>
+          <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <h1 className="textBetween">
+              Co-Ordinator Information
+            </h1>
+            </div>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <Grid 
+              container
+              style={{
+                border: "0.5px solid #D3E2DA",
+                borderRadius:"25px",
+                width: "80%",
+                display: 'flex',
+                // alignSelf:'center',
+                alignItems:'center',
+                justifyContent:'center',
+                minHeight: '90px',
+                // paddingTop: '10px',
+                paddingLeft: '120px',
+                // paddingRight: '10px',
+                // paddingBottom: '30px'
+              }}
+            >
+              <Grid item xs={6}>
+                <p style={{fontSize: '20px'}}>
+                  <b style={{fontWeight:600}}>Name : </b>
+                  {'   '}
+                  Nikita Sopan Tipule
+                </p>
+              </Grid>
+              <Grid item xs={6}>
+                <p style={{fontSize: '20px'}}>
+                  <b style={{fontWeight:600}}>Email : </b>
+                  {'   '}
+                    faculty@gamil.com
+                </p>
+                </Grid>
+              <Grid item xs={6}>
+                <p style={{fontSize: '20px'}}>
+                  <b style={{fontWeight:600}}>Mis : </b>
+                  {'   '}
+                    11111111111
+                </p>
+                </Grid>
+              <Grid item xs={6}>
+                <p style={{fontSize: '20px'}}>
+                  <b style={{fontWeight:600}}>Department: </b>
+                  {'   '}
+                    Computer Engineering
+                </p>
+                </Grid>
+            </Grid>
+            </div>
+          </div>
+          
+        </div>
+        {/* <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginBottom: '0px', marginTop: '20px'}}>
+            <h1 className="textBetween">
+              List of All Students
+            </h1>
+            </div> */}
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px', marginBottom: '50px'}}>
+          
+          <Paper sx = {{width: '100%','@media screen and (min-width: 40em)': {width: '80%'}, overflow: 'hidden'}}>
+            <TableContainer sx={{ maxHeight: 500 }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead >
+                  <TableRow>
+                    {this.columns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align='center'
+                        style={{ minWidth: column.minWidth, backgroundColor: 'ButtonHighlight', color:'black' }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.rows
+                    .slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage)
+                    .map((row) => {
+                      return (
+                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                          {this.columns.map((column) => {
+                            const value = row[column.id];
+                            return (
+                              <TableCell key={column.id} align='center'>
+                                {column.id === 'status'? (
+                                <div>
+                                {column.id === 'status' && value === 'Verified'
                                   ? (
-                                    <div style = {{color: 'red'}}>{value}</div>
+                                    <div style = {{color: 'green'}}>{value}</div>
                                   ):(
-                                    <div style = {{color: 'blue'}}>{value}</div>
-                                  )
-                                  }
-                                  </div>    
-                                )}
-                                </div>
-                                ):(
-                                  <div>
-                                     {/* to do Link part */}
-                                    <Link to={{pathname: '/'}} style={{textDecoration: 'none', color: 'black'}} >
-                                    {value}
-                                    </Link>
+                                    <div>
+                                    {column.id === 'status' && value === 'Not Verified'
+                                    ? (
+                                      <div style = {{color: 'red'}}>{value}</div>
+                                    ):(
+                                      <div style = {{color: 'blue'}}>{value}</div>
+                                    )
+                                    }
+                                    </div>    
+                                  )}
                                   </div>
-                                )}
-                            </TableCell>
-                          );
-                        })}
-                       
-                      </TableRow>
-                    );
-                  })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={this.rows.length}
-            rowsPerPage={this.state.rowsPerPage}
-            page={this.state.page}
-            onPageChange={this.handleChangePage}
-            onRowsPerPageChange={this.handleChangeRowsPerPage}
-          />
-        </Paper>
+                                  ):(
+                                    <div>
+                                      {/* to do Link part */}
+                                      <Link to={{pathname: '/'}} style={{textDecoration: 'none', color: 'black'}} >
+                                      {value}
+                                      </Link>
+                                    </div>
+                                  )}
+                              </TableCell>
+                            );
+                          })}
+                        
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={this.rows.length}
+              rowsPerPage={this.state.rowsPerPage}
+              page={this.state.page}
+              onPageChange={this.handleChangePage}
+              onRowsPerPageChange={this.handleChangeRowsPerPage}
+            />
+          </Paper>
+        </div>
       </div>
     </>
   );
