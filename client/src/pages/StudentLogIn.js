bf1b3128b6a4285522007260d6c73068ef072031
Deepakshi <deepakshisinghblouria@gmail.com>
2021-09-19 20:32:36 +0530

    improv: student login

--- client/src/pages/StudentLogIn.js
+++ client/src/pages/StudentLogIn.js
@@ -1,4 +1,4 @@
-import {React} from 'react';
+import {React, useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,18 +10,54 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import NavBar from '../components/Navbar/Navbar';
+import {useForm, Form} from './Form';
+import Input from './Input';
 const theme = createTheme();
-
+const initialFValues = {
+  id: 0,
+  email: '',
+  password: '',
+}
 export default function StudentLogIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
+   
+   const [recordForEdit, setRecordForEdit]= useState(null)
+   const addorEdit=(user, resetForm) => {
+    resetForm()
+    setRecordForEdit(null)
+    }
+  const validate = (fieldValues = values) => {
+    let temp = {...errors} 
+    if('email' in fieldValues)
+      (temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid.") || (temp.email = fieldValues.email ? "" : "This field is required.")
+    if('password' in fieldValues)
+      temp.password = fieldValues.password ? "" : "This field is required."
+    setErrors({
+        ...temp
+    })
+    if(fieldValues == values)
+     return Object.values(temp).every(x => x == "")
+    }
+    const {
+      values,
+      setValues,
+      errors,
+      setErrors,
+      handleInputChange,
+      resetForm
+     } = useForm(initialFValues,true,validate);
+     const handleSubmit = e => {
+      e.preventDefault()
+      if(validate()){
+          addorEdit(values, resetForm);
+          const data ={
+              fullname: values.fullname,
+              email: values.email,
+              password: values.password,
+          }
+          console.log(data)
+      }
+      
+  }
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
@@ -40,48 +76,44 @@ export default function StudentLogIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2}}
-            >
-              Sign In
-            </Button>
-            <Grid container spacing={0}>
-              <Grid item xs>
-              <Link href="/" variant="body2">
-                  {"Home Page"}
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/Register" variant="body2">
-                  {"Don't have an account? Register"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+          <Form onSubmit = {handleSubmit}>
+                <Grid align='center' xs={12}>
+                    <Grid align='center' item xs={12}>
+                      <Input 
+                        name="email"
+                        label="Email*"
+                        value={values.email}
+                        onChange={handleInputChange}
+                        error={errors.email}
+                      />
+                     <Input 
+                        name="password"
+                        label="Password*"
+                        value={values.password}
+                        onChange={handleInputChange}
+                        error={errors.password}
+                      />
+                      <Grid item xs={12}>
+                      <Button
+                       type='submit'
+                       variant="contained"
+                       sx={{ mt: 3, mb: 2}}
+                       onClick={handleSubmit}
+                       style={{width: "100%", marginLeft:'2%'}}
+                       >
+                        SIGN IN
+                      </Button>
+                      </Grid>
+                     
+                       <Grid item xs>
+                       <Link href="/Register" variant="body2">
+                         {"Don't have an account? Register"}
+                       </Link>
+                       </Grid>
+                      </Grid>
+                    </Grid>
+            </Form>
+         
         </Box>
       </Container>
     </ThemeProvider>
