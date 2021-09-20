aa5781d3beceac6eebfeb38a7788cd7351171b0a
vishwajit2 <vishwajitkadam13@gmail.com>
2021-09-21 00:11:25 +0530

    common staff functionality

---
+++ routes/staff.js
@@ -0,0 +1,17 @@
+// routes common to nonStudent users
+const { loginStaff } = require("../controllers/auth");
+const { auth } = require("../middleware/auth");
+const { myProfileStaff } = require("../controllers/staff");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/login", loginStaff);
+
+router.get("/me", [auth, myProfileStaff]);
+
+router.get("/getuser", auth, (req, res) => {
+  return res.status(200).json({ userId: req.userId, userRole: req.userRole });
+});
+
+module.exports = router;
