5cb441b65050df87e720f567aeccad30846f8f39
vishwajit2 <vishwajitkadam13@gmail.com>
2021-09-22 00:56:57 +0530

    feat: add/remove phd cords

--- routes/phdCord.js
+++ routes/phdCord.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const { loginPhdCord } = require("../controllers/auth");
+const {
+  getPhdCordInfo,
+  addPhdCord,
+  removePhdCord,
+} = require("../controllers/phdCord");
+const { auth } = require("../middleware/auth");
 const router = express.Router();
 
-router.post("/login", loginPhdCord);
+router.get("/:cordId", auth, getPhdCordInfo);
+
+router.post("/add", auth, addPhdCord);
+
+router.delete("/remove/:cordId", auth, removePhdCord);
+
 module.exports = router;
