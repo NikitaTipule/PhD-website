1199a4b126dc8166d113ac3c69b7e40133cbf856
vishwajit2 <vishwajitkadam13@gmail.com>
2021-10-07 14:07:54 +0530

    admission program details

---
+++ client/src/phdAdmDetails.js
@@ -0,0 +1,21 @@
+export const departmentNames = {
+  civil: "Civil Engineering",
+  computer: "Computer Engineering",
+  electrical: "Electrical Engineering",
+  entc: "Electronics & Telecommunication Engineering",
+  instru: "Instrumentation & Control Engineering",
+  mechanical: "Mechanical Engineering",
+  metallurgy: "Metallurgical Engineering",
+  manufacturing: "Production Engineering",
+};
+
+export const departmentNamesList = [
+  "Civil Engineering",
+  "Computer Engineering",
+  "Electrical Engineering",
+  "Electronics & Telecommunication Engineering",
+  "Instrumentation & Control Engineering",
+  "Mechanical Engineering",
+  "Metallurgical Engineering",
+  "Production Engineering",
+];
