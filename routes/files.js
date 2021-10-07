8e2ae614a68afea4e8e6a456cade147b7808b35c
vishwajit2 <vishwajitkadam13@gmail.com>
2021-10-07 11:49:01 +0530

    fix: missing export

--- routes/files.js
+++ routes/files.js
@@ -1,4 +1,6 @@
 const { auth } = require("../middleware/auth");
+const express = require("express");
+const router = express.Router();
 
 const {
   uploadFile,
@@ -11,3 +13,5 @@ router.post("/upload", [auth, uploadFile]);
 router.get("/get/:filename", [auth, getFileGrid]);
 
 router.delete("/delete/:filename", [auth, removeFileGrid]);
+
+module.exports = router;
