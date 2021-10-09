1a85dec04c1e9601b1004bd3fe472d970f0dc5ee
vishwajit2 <vishwajitkadam13@gmail.com>
2021-10-09 17:44:41 +0530

    fix: user data access

--- controllers/staff.js
+++ controllers/staff.js
@@ -4,8 +4,8 @@ const roleToModel = require("./roles");
 
 exports.myProfileStaff = (req, res) => {
   if (!req.userId) res.status(400).json({ error: "id is required" });
-  const User = roleToModel[userRole];
-  User.findById(id, (err, user) => {
+  const User = roleToModel[req.userRole];
+  User.findById(req.userId, (err, user) => {
     if (err) {
       return res.status(404).json({ error: "user doesn't exist" });
     }
