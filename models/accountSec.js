1884fb796e9d7928ed97a2d24d02d262ef014629
vishwajit2 <vishwajitkadam13@gmail.com>
2021-10-14 14:02:58 +0530

    replace LDAP auth with email for account section

--- models/accountSec.js
+++ models/accountSec.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-const { reqString, email } = require("./schemaFields");
+const { reqString, email, preSaveHashPassword } = require("./schemaFields");
 
 const accountSecSchema = mongoose.Schema({
   name: reqString,
-  mis: reqString,
   email: email,
+  password: { type: String },
 });
 
+accountSecSchema.pre("save", preSaveHashPassword);
 const AccountSec = mongoose.model("accountSec", accountSecSchema, "accountSec");
 
 module.exports = AccountSec;
