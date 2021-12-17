ab2e2d75b41da693e6b650a445c47aec13270883
vishwajit2 <vishwajitkadam13@gmail.com>
2021-12-17 15:45:23 +0530

    add UI to submit OTPs

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,26 +15,34 @@ import { BACKEND_URL } from "../config";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import CircularProgress from "@mui/material/CircularProgress";
+import OTP from "./otp";
 
 const theme = createTheme();
 const initialFValues = {
   fullname: "",
   email: "",
+  mobile: "",
   password: "",
   cpassword: "",
 };
 export default function Register() {
   let history = useHistory();
   const [loading, setLoading] = useState(false);
-
+  const [userId, setUserId] = useState(null);
   const validate = (fieldValues = values) => {
     let temp = { ...errors };
     for (const key in fieldValues) {
       temp[key] = fieldValues[key] ? "" : "This field is required.";
     }
-    const { email, cpassword } = fieldValues;
+    const { email, mobile, cpassword } = fieldValues;
     if (email && !/$^|.+@.+..+/.test(email)) {
       temp.email = "Email is not valid.";
+    }
+    if (
+      mobile &&
+      !new RegExp("^([0|+[0-9]{1,5})?([7-9][0-9]{9})$").test(mobile)
+    ) {
+      temp.mobile = "mobile is not valid";
     }
     if (cpassword && cpassword !== values.password) {
       temp.cpassword = "Passwords do not match. ";
@@ -55,6 +63,7 @@ export default function Register() {
       const data = {
         name: values.fullname,
         email: values.email,
+        mobile: values.mobile,
         password: values.password,
       };
       console.log(data);
@@ -64,8 +73,8 @@ export default function Register() {
         .post(url, data)
         .then((res) => {
           setLoading(false);
-          alert("Registration Successful, Please verify your email");
-          history.push("/login/candidate");
+          alert("Registration Successful, Please verify mobile and email");
+          setUserId(res.data.userId);
         })
         .catch((err) => {
           setLoading(false);
@@ -73,80 +82,92 @@ export default function Register() {
         });
     }
   };
-  return (
-    <ThemeProvider theme={theme}>
-      <NavBar loggedin={false}/>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "cadetblue" }}></Avatar>
-          <Typography component="h1" variant="h5">
-            Register
-          </Typography>
-          <Form onSubmit={handleSubmit}>
-            <Grid align="center" xs={12}>
-              <Grid align="center" item xs={12}>
-                <Input
-                  name="fullname"
-                  label="Full Name*"
-                  value={values.fullname}
-                  onChange={handleInputChange}
-                  error={errors.fullname}
-                />
-                <Input
-                  name="email"
-                  label="Email*"
-                  value={values.email}
-                  onChange={handleInputChange}
-                  error={errors.email}
-                />
-                <Input
-                  name="password"
-                  label="Password*"
-                  value={values.password}
-                  onChange={handleInputChange}
-                  error={errors.password}
-                />
-                <Input
-                  name="cpassword"
-                  label="Confirm Password*"
-                  value={values.cpassword}
-                  onChange={handleInputChange}
-                  error={errors.cpassword}
-                />
-                <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    onClick={handleSubmit}
-                    style={{ width: "100%", marginLeft: "2%" }}
-                  >
-                    {loading ? (
-                      <CircularProgress size="1.7em" color="inherit" />
-                    ) : (
-                      "Register"
-                    )}
-                  </Button>
-                </Grid>
 
-                <Grid item xs>
-                  <Link href="/StudentLogIn" variant="body2">
-                    {"Already Registered? Log In"}
-                  </Link>
+  if (userId) {
+    return <OTP userId={userId} />;
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <NavBar loggedin={false} />
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "cadetblue" }}></Avatar>
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+            <Form onSubmit={handleSubmit}>
+              <Grid align="center" xs={12}>
+                <Grid align="center" item xs={12}>
+                  <Input
+                    name="fullname"
+                    label="Full Name*"
+                    value={values.fullname}
+                    onChange={handleInputChange}
+                    error={errors.fullname}
+                  />
+                  <Input
+                    name="email"
+                    label="Email*"
+                    value={values.email}
+                    onChange={handleInputChange}
+                    error={errors.email}
+                  />
+                  <Input
+                    name="mobile"
+                    label="mobile*"
+                    value={values.mobile}
+                    onChange={handleInputChange}
+                    error={errors.mobile}
+                  />
+                  <Input
+                    name="password"
+                    label="Password*"
+                    value={values.password}
+                    onChange={handleInputChange}
+                    error={errors.password}
+                  />
+                  <Input
+                    name="cpassword"
+                    label="Confirm Password*"
+                    value={values.cpassword}
+                    onChange={handleInputChange}
+                    error={errors.cpassword}
+                  />
+                  <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                      onClick={handleSubmit}
+                      style={{ width: "100%", marginLeft: "2%" }}
+                    >
+                      {loading ? (
+                        <CircularProgress size="1.7em" color="inherit" />
+                      ) : (
+                        "Register"
+                      )}
+                    </Button>
+                  </Grid>
+
+                  <Grid item xs>
+                    <Link href="/StudentLogIn" variant="body2">
+                      {"Already Registered? Log In"}
+                    </Link>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </Form>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+            </Form>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
---
+++ client/src/pages/otp.js
@@ -0,0 +1,135 @@
+import { React, useState } from "react";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import NavBar from "../components/Navbar/Navbar";
+import { Form } from "./Form";
+import Input from "./Input";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
+import { Redirect } from "react-router";
+
+const theme = createTheme();
+
+export default function OTP(props) {
+  const [mailotp, setMailotp] = useState("");
+  const [mobileotp, setMobileotp] = useState("");
+  const [mailVerified, setMailVerified] = useState(false);
+  const [mobileVerified, setMobileVerified] = useState(false);
+  const submitMailOtp = (e) => {
+    e.preventDefault();
+    if (mailotp && mailotp.length === 6) {
+      axios
+        .post(BACKEND_URL + "/students/verifymail", {
+          userId: props.userId,
+          otp: mailotp,
+        })
+        .then((res) => {
+          setMailVerified(true);
+          alert("Email Verified");
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("Invalid details");
+        });
+    } else alert("Invalid details");
+  };
+
+  const submitMobileOtp = (e) => {
+    e.preventDefault();
+    if (mobileotp && mobileotp.length === 6) {
+      axios
+        .post(BACKEND_URL + "/students/verifymobile", {
+          userId: props.userId,
+          otp: mobileotp,
+        })
+        .then((res) => {
+          setMobileVerified(true);
+          alert("Mobile Verified");
+        })
+        .catch((err) => {
+          console.log(err);
+          alert("Invalid details");
+        });
+    } else alert("Invalid details");
+  };
+
+  if (mailVerified && mobileVerified) {
+    return <Redirect from="/Register" to="/login/candidate" />;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <NavBar loggedin={true} />
+      <Container component="main" maxWidth="xs">
+        {/* <CssBaseline /> */}
+        <Box
+          sx={{
+            marginTop: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {/* <Avatar sx={{ m: 1, bgcolor: "cadetblue" }}></Avatar> */}
+          <Typography component="h1" variant="h5">
+            Verify
+          </Typography>
+          <br />
+          <form>
+            <Grid align="center">
+              <Grid align="center" item xs={12}>
+                <Input
+                  name="Email OTP"
+                  label="Email OTP*"
+                  value={mailotp}
+                  onChange={(e) => setMailotp(e.target.value)}
+                />
+                <Grid item xs={12}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={submitMailOtp}
+                    style={{ width: "100%", marginLeft: "2%" }}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+
+                <Grid align="center" item xs={12}>
+                  <Input
+                    name="Mobile OTP"
+                    label="Monile OTP*"
+                    value={mobileotp}
+                    onChange={(e) => setMobileotp(e.target.value)}
+                  />
+                  <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                      onClick={submitMobileOtp}
+                      style={{ width: "100%", marginLeft: "2%" }}
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
+
+                  <Grid item xs>
+                    <Link href="/Admin" variant="body2">
+                      {"Go to dashboard"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
