945454c2b464efdb97f0559d806bc4127b205c08
Pragati Narote <pragatinarote11@gmail.com>
2022-01-08 00:29:40 +0530

    fix: minor changes

--- client/src/components/linkComponent.js
+++ client/src/components/linkComponent.js
@@ -1,28 +1,30 @@
-import React , {useState} from 'react';
-import { CustomDialog, useDialog } from 'react-st-modal';
+import React, { useState } from "react";
+import { CustomDialog, useDialog } from "react-st-modal";
 import Button from "@mui/material/Button";
+
 function CustomDialogContent() {
-    const dialog = useDialog();
-  
-    const [value, setValue] = useState();
-  
-    return (
-      <div style={{alignContent:"center",alignItems:"center",padding:"10px"}}>
+  const dialog = useDialog();
+  const [value, setValue] = useState();
+
+  return (
+    <div
+      style={{ alignContent: "center", alignItems: "center", padding: "10px" }}
+    >
       <div>Enter the title</div>
-        <input
-          type="text"
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
-        <div>Enter the Link</div>
-        <input
-          type="text"
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
-        <div style={{marginTop:"10px"}}>
+      <input
+        type="text"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <div>Enter the Link</div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <div style={{ marginTop: "10px" }}>
         <Button
           onClick={() => {
             // Сlose the dialog and return the value
@@ -32,9 +34,9 @@ function CustomDialogContent() {
         >
           Submit
         </Button>
-        </div>
       </div>
-    );
-};
+    </div>
+  );
+}
+
 export default CustomDialogContent;
-  
--- client/src/pages/Link.js
+++ client/src/pages/Link.js
@@ -1,30 +1,47 @@
 import { Component, React } from "react";
 import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import { TextField, Typography } from "@material-ui/core";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import NavBar from "../components/Navbar/Navbar";
-import { useForm, Form } from "./Form";
-import Input from "./Input";
 import { BACKEND_URL } from "../config";
 import axios from "axios";
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
 import "../CSS/links.css";
-import CustomDialogContent from "../components/linkComponent";
-import { CustomDialog, useDialog } from 'react-st-modal';
 // import Box from "@material-ui/core/Box";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
+
 class AddLink extends Component {
   constructor(props) {
     super(props);
     this.state = {
       links: [],
+      title: "",
+      link: "",
       token: "",
     };
   }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addLink = () => {
+    const data = {
+      title: this.state.title,
+      link: this.state.link,
+    };
+    try {
+      axios
+        .post(BACKEND_URL + "/phdCords/addlink", data, {
+          headers: { "phd-website-jwt": this.state.token },
+        })
+        .then((res) => {
+          console.log("Link Added");
+        });
+    } catch (err) {
+      console.log(err.res);
+    }
+  };
 
   async componentDidMount() {
     if (localStorage.getItem("phd-website-jwt")) {
@@ -48,43 +65,89 @@ class AddLink extends Component {
   }
 
   render() {
-    
-
     return (
       <>
         <NavBar loggedin={true} />
-        <Container component="main" max-width="sm" style={{ marginTop: "90px"}}>
-        <h1 style={{alignItems:"center",textAlign:"center" }}>Links</h1>
-        <Button
-        onClick={async () => {
-          const result = await CustomDialog(<CustomDialogContent />, {
-            title: 'Custom Dialog',
-            showCloseIcon: true,
-          });
-        }}
-        variant="contained"
-      >
-        <AddIcon />Add Link
-      </Button>
-        <div class="container-tab">
-          <ul class="responsive-table">
-          <li class="table-header item-head">
-            <div class="col col-1">Title</div>
-            <div class="col col-2">Links</div>
-            <div class="col col-3"></div>
-            
-          </li>
-          {this.state.links.map((item)=>(
-            <li class="table-row item-tab">
-            <div class="col col-1" data-label="Title">{item.title}</div>
-            <div class="col col-2" data-label="Link"><a href={{}}>{item.link}</a></div>
-            <Button><div class="col col-3" data-label=""><DeleteIcon /></div></Button>
-            </li>
-          ))}
-          
-          
-          </ul>
-       </div>
+        <Container
+          component="main"
+          max-width="sm"
+          style={{ marginTop: "90px" }}
+        >
+          <h1 style={{ alignItems: "center", textAlign: "center" }}>Links</h1>
+          <div className="inputsRow">
+            <div style={{ width: "500px", margin: "3px" }}>
+              <Typography>Title for Link</Typography>
+              <TextField
+                disabled={this.state.disabled}
+                className="mb-3"
+                fullWidth
+                onChange={this.handleChange}
+                value={this.state.title}
+                name="title"
+                label="Title"
+                variant="outlined"
+                required="true"
+                style={{ marginTop: "2px" }}
+              />
+              {/* {this.state.errorMotherName && (
+                <div style={{ color: "red" }}>
+                  <Typography>Mother's Name is required field</Typography>
+                </div>
+              )} */}
+            </div>
+            <div style={{ width: "500px", margin: "3px" }}>
+              <Typography>Link</Typography>
+              <TextField
+                disabled={this.state.disabled}
+                className="mb-3"
+                fullWidth
+                onChange={this.handleChange}
+                value={this.state.link}
+                name="link"
+                label="Link"
+                variant="outlined"
+                required="true"
+                style={{ marginTop: "2px" }}
+              />
+              {/* {this.state.errorMotherName && (
+                <div style={{ color: "red" }}>
+                  <Typography>Mother's Name is required field</Typography>
+                </div>
+              )} */}
+            </div>
+            <Button
+              onClick={this.addLink}
+              variant="contained"
+              style={{ height: "50px", margin: "3px" }}
+            >
+              <AddIcon />
+              Add Link
+            </Button>
+          </div>
+          <div class="container-tab">
+            <ul class="responsive-table">
+              <li class="table-header item-head">
+                <div class="col col-1">Title</div>
+                <div class="col col-2">Links</div>
+                <div class="col col-3"></div>
+              </li>
+              {this.state.links.map((item) => (
+                <li class="table-row item-tab">
+                  <div class="col col-1" data-label="Title">
+                    {item.title}
+                  </div>
+                  <div class="col col-2" data-label="Link">
+                    <a href={{}}>{item.link}</a>
+                  </div>
+                  <Button>
+                    <div class="col col-3" data-label="">
+                      <DeleteIcon />
+                    </div>
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </Container>
       </>
     );
